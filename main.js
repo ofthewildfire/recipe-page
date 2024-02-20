8cbe89408a6ed9e4c4c05ea27b6bede3903570d8
ofthewildfire <103148299+ofthewildfire@users.noreply.github.com>
2024-02-20 23:36:18 +0200

    dark mode created

--- main.js
+++ main.js
@@ -5,3 +5,40 @@ import "./modernreset.css"
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
+
+
+// Dark mode toggle 
+
+const body = document.getElementById("body-el");
+const main = document.querySelector(".main-content")
+const h2s = document.querySelectorAll("h2")
+const prep = document.querySelector(".main_recipe--prep")
+const h1 = document.querySelector('.recipe__title')
+const ol = document.querySelectorAll('ol > li')
+const prepTitle = document.querySelector('.recipe-prep__title')
+
+const toggle = document.querySelector(".toggle");
+
+toggle.addEventListener("click", toggleColor);
+
+const darkBg =  "dark-bg"
+const darkCard = "dark-card"
+const darkTitles = "dark-titles"
+const darkPrep = "dark-prep"
+const darkRecipleTitle = "dark-recipe-title"
+const darkLiMarker = 'dark-li-marker'
+const darkPrepTitle = "dark-prep-title"
+
+function toggleColor() {
+h1.classList.toggle(darkRecipleTitle)
+prep.classList.toggle(darkPrep)
+body.classList.toggle(darkBg)
+main.classList.toggle(darkCard)
+h2s.forEach((titles) => {
+  titles.classList.toggle(darkTitles)
+})
+ol.forEach((lis) => {
+  lis.classList.toggle(darkLiMarker)
+})
+prepTitle.classList.toggle(darkPrepTitle)
+}
