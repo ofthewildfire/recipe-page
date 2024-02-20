d725e89e15499e64aae7806c17c9ab3bee321c61
ofthewildfire <103148299+ofthewildfire@users.noreply.github.com>
2024-02-20 21:46:57 +0200

    Finished challenge

---
+++ main.js
@@ -0,0 +1,7 @@
+// Supports weights 100-900
+
+import '@fontsource-variable/outfit';
+import "./modernreset.css"
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
